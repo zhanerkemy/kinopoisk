61dc1821ad50f696bee996a6064ce8bf85f9f918
zhanerkemy <janerke16.05.2005@gmail.com>
2024-10-02 00:40:23 +0500

    modernization for user page

--- server/pages/router.js
+++ server/pages/router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const Genres = require('../Genres/Genres')
 const Country = require('../Country/Country')
+const User = require('../auth/User')
 
 router.get('/', async(req, res) => {
     const allGenres = await Genres.find()
@@ -16,8 +17,15 @@ router.get('/register', (req, res) => {
     res.render("register", {user: req.user ? req.user : {}})
 })
 
-router.get('/profile/:id', (req, res) => {
-    res.render("profile", {user: req.user ? req.user : {}})
+router.get('/profile/:id', async (req, res) => {
+    const allGenres = await Genres.find() 
+    const user = await User.findById(req.params.id)
+    if(user){
+        res.render("profile", {user: user, genres: allGenres, loginUser: req.user}) //loginUser gives information about in whose account we are
+    } else{
+        res.redirect('/not-found')
+    }
+    
 })
 
 router.get('/admin', (req, res) => {
@@ -34,6 +42,10 @@ router.get('/edit', async(req, res) => {
     const allGenres = await Genres.find()
     const allCountries = await Country.find()
     res.render("editFilm", {genres: allGenres, countries: allCountries, user: req.user ? req.user : {}})
+})
+
+router.get('/not-found', (req, res) => {
+    res.render("notFound")
 })
 
 module.exports = router
