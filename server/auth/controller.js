0d4f2932a3bac761a70d6b9302ea62113fb76f9d
zhanerkemy <janerke16.05.2005@gmail.com>
2024-10-01 23:52:10 +0500

    logout from account

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -34,7 +34,17 @@ const signIn = (req, res) => {
     res.redirect(`/profile/${req.user._id}`)
 }
 
+const signOut = (req, res) => {
+    req.logout(function(err){
+        if(err){
+            console.log(err);
+        }
+        res.redirect('/')
+    })
+}
+
 module.exports = { 
     signUp,
-    signIn
+    signIn,
+    signOut
 }
--- server/auth/router.js
+++ server/auth/router.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router();
-const {signUp, signIn} = require('./controller')
+const {signUp, signIn, signOut} = require('./controller')
 
 router.post('/api/signup', signUp)
 router.post('/api/signin', passport.authenticate('local', {failureRedirect: '/login?error=1'}), signIn)
-
+router.get('/api/signout', signOut)
 module.exports = router
