60ed27d181e586fb81e6f27a213542595799b25b
zhanerkemy <janerke16.05.2005@gmail.com>
2024-10-06 16:36:09 +0500

    google authentication using google cloud

--- server/auth/router.js
+++ server/auth/router.js
@@ -7,5 +7,8 @@ const createAdmin = require('../Admin/seed')
 router.post('/api/signup', signUp)
 router.post('/api/signin', passport.authenticate('local', {failureRedirect: '/login?error=1'}), signIn)
 router.get('/api/signout', signOut)
+router.get('/api/auth/google', passport.authenticate('google'), (req, res) => {
+    res.redirect('/profile/' + req.user._id)
+})
 createAdmin()
 module.exports = router
--- server/config/passport.js
+++ server/config/passport.js
@@ -2,6 +2,9 @@ const passport = require('passport')
 const User = require('../auth/User')
 const bcrypt = require('bcrypt')
 const LocalStrategy = require('passport-local')
+const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+//Client id: 372397368408-6ko206j1e16uusk7qgv0b0f0qnmuiqff.apps.googleusercontent.com
+//Client secret: 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
 
 passport.use(new LocalStrategy(
     {
@@ -21,6 +24,25 @@ passport.use(new LocalStrategy(
         }).catch(e => {
             return done(e)
         })
+    }
+));
+
+passport.use(new GoogleStrategy({
+    clientID:     '372397368408-6ko206j1e16uusk7qgv0b0f0qnmuiqff.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: "http://localhost:8000/api/auth/google",
+    scope: ['openid', 'email', 'profile'],
+    passReqToCallback   : true
+  },
+  async function(request, accessToken, refreshToken, profile, done) {
+    const user = await User.find({googleId: profile.id})
+    console.log(profile);
+    const newUser = await new User({
+        googleId: profile.id,
+        full_name: profile.displayName,
+        email: profile.emails[0].value
+    }).save()
+    return done(null, newUser);
     }
 ));
 
