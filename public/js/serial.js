492fde68095241814d7663fef209806d6b674d7f
zhanerkemy <janerke16.05.2005@gmail.com>
2024-10-06 18:56:56 +0500

    add series finalize

--- public/js/serial.js
+++ public/js/serial.js
@@ -3,14 +3,17 @@ let addSeriesBtn = document.querySelector('#addSeriesBtn')
 let urls = document.querySelector('#urls')
 function addSeries(){
     addFilmUrlBtn.style.display = "none"
-    let output = urls.innerHTML
     let number = document.querySelectorAll('.series')
-    output += 
-    `<div class="series">
-        <p>${number.length + 1} серия</p>
-        <input type="text" placeholder="Введите ссылку на видео">
-    </div>`
-    urls.innerHTML = output
+    let series = document.createElement('div')
+    series.classList.add("series")
+    let title = document.createElement('p')
+    let input = document.createElement('input')
+    title.innerHTML = number.length + 1 + 'серия'
+    input.name = "series"
+    input.placeholder = "Введите ссылку на видео"
+    series.append(title)
+    series.append(input)
+    urls.append(series)
 }
 
 function addFilmUrl(){
